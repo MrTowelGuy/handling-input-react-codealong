d9570364670421c475ee5bd5e7d1e2520ec45c45
Jaden Garrity <110641058+MrTowelGuy@users.noreply.github.com>
2022-09-23 10:42:50 -0400

    made it so text can type in the input field

--- src/App.js
+++ src/App.js
@@ -3,18 +3,30 @@ import "./styles.css";
 
 export default function App() {
   const [state, setState] = useState({
-    skills: [{ skill: "JavaScript", level: "4" }]
+    skills: [{ skill: "JavaScript", level: "4" }],
+    skill: "",
+    level: "3"
   });
 
   function handleSubmit() {
     alert("ADD SKILL CLICKED");
   }
 
+  function handleChange(e) {
+    //e is Representative  of event
+    //target is the inout field and the value is whatever we are typing in the input field. event is whatever event takes place. target value is just what the button that is pushed essentially.
+    setState(function (prevState) {
+      return { ...prevState, skill: e.target.valye };
+    });
+  }
+
   return (
     <section>
       <h2>DEV SKILLS</h2>
       <hr />
-      {state.skills.map((s) => (
+      {state.skills.map((
+        s //array called skills, and we are mapping it. for every skill that we called "s" we are putting in a separate div
+      ) => (
         <article key={s.skill}>
           <div>{s.skill}</div> <div>{s.level}</div>
         </article>
@@ -23,11 +35,11 @@ export default function App() {
       <form>
         <label>
           <span>SKILL</span>
-          <input name="skill" />
+          <input name="skill" value={state.skill} onChange={handleChange} />
         </label>
         <label>
           <span>LEVEL</span>
-          <select name="level">
+          <select name="level" value={state.level}>
             <option value="1">1</option>
             <option value="2">2</option>
             <option value="3">3</option>
